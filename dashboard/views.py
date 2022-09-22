38dec82d39679cf617f6014fa7b3f5625f1cdabf
Albert Lei <albert.lei@hotmail.com>
2022-09-22 23:27:11 +0800

    build the framework for the website

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,4 +4,4 @@ from django.http.response import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello Dashboard')
-    return render(request,'index.html')
+    return render(request,'dashboard/home.html')
